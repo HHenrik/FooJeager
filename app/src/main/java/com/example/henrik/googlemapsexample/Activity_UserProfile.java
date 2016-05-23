ff5e0a8a40c3b0e9218e9407a834a6118524a6cc
Scrubzilla <nicklas.brisendal@hotmail.com>
2016-05-23 02:01:32 +0200

    -Added a alert dialog window design -Added a first time startup demand to enter a username *Only for profile page for now -Prepared for database handling -Implemented simple level calculation

--- app/src/main/java/com/example/henrik/googlemapsexample/Activity_UserProfile.java
+++ app/src/main/java/com/example/henrik/googlemapsexample/Activity_UserProfile.java
@@ -1,8 +1,16 @@
 package com.example.henrik.googlemapsexample;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -10,21 +18,93 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class Activity_UserProfile extends AppCompatActivity {
-    ArrayList<ReviewObject> recentReviewsList = new ArrayList();
-    ListAdapter adapter;
+    private ArrayList<ReviewObject> recentReviewsList = new ArrayList();
+    private ListAdapter adapter;
 
+    private String SAVED_INFO = "storedAccountSettings";
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+
+    private String androidId;
+    private boolean firstAppStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
-        String androidId = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
 
-        updateUserName(androidId);
-        updateLikes("100");
-        updateDislikes("50");
-        updateLevel("50");
-        setRecentReviews();
+        preferences = getSharedPreferences(SAVED_INFO, MODE_PRIVATE);
+        editor = preferences.edit();
+
+        firstAppStart = preferences.getBoolean("firstAppStart", true);
+
+        if(firstAppStart == true){
+            androidId = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
+            createAlertDialog();
+        }
+        else{
+            updateUserName(preferences.getString("userName", "-"));
+            updateLikes("80");
+            updateDislikes("40");
+
+            int level = (Integer.parseInt("80") - Integer.parseInt("40")) / 10;
+            updateLevel(String.valueOf(level));
+
+            setRecentReviews();
+        }
+
+    }
+
+    private void createAlertDialog(){
+        // get prompts.xml view
+        LayoutInflater li = LayoutInflater.from(this);
+        View promptsView = li.inflate(R.layout.prompt, null);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        // set prompts.xml to alertdialog builder
+        alertDialogBuilder.setView(promptsView);
+
+        final EditText userInput = (EditText) promptsView.findViewById(R.id.editTextDialogUserInput);
+
+        // set dialog message
+        alertDialogBuilder.setCancelable(false)
+
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        editor.putString("userName", userInput.getText().toString());
+                        editor.putString("androidID", androidId);
+                        editor.putBoolean("firstAppStart", false);
+                        editor.commit();
+                        //getDataFromDB
+                        updateUserName(preferences.getString("userName", "-"));
+                        updateLikes("80");
+                        updateDislikes("40");
+
+                        int level = (Integer.parseInt("80") - Integer.parseInt("40")) / 10;
+                        updateLevel(String.valueOf(level));
+
+                        setRecentReviews();
+                    }
+                })
+
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                editor.putBoolean("firstAppStart", true);
+                                editor.commit();
+                                dialog.cancel();
+                            }
+                        });
+
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        // show it
+        alertDialog.show();
+        alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(Color.parseColor("#F4F4F4"));
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(Color.parseColor("#F4F4F4"));
+        alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0xFF107896));
 
     }
 
@@ -62,4 +142,5 @@ public class Activity_UserProfile extends AppCompatActivity {
 
         recentReviews.setAdapter(adapter);
     }
+
 }
