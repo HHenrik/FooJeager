7c7c3596639acabff083d1f3d12c20971355bea2
HHenrik <henrikasman@yahoo.se>
2016-05-27 09:38:28 +0200

    fixed minor changes

--- app/src/main/java/com/example/henrik/googlemapsexample/review/ReviewViewer.java
+++ app/src/main/java/com/example/henrik/googlemapsexample/review/ReviewViewer.java
@@ -3,7 +3,6 @@ package com.example.henrik.googlemapsexample.review;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListAdapter;
@@ -128,7 +127,9 @@ public class ReviewViewer extends AppCompatActivity {
 
     @Override
     public void onResume() {
+
         super.onResume();  // Always call the superclass method first
+        if(googleReview == false)
         getRestaurantReviews(restaurantId);
 
     }
